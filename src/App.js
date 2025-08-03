16a42c1f77ea2ce2a8dc1b5f1ef885adcba0a8ae
hsuhagiyafintech <harikrushna.suhagiya@fintechglobal.center>
2025-08-03 15:58:05 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import FileUpload from "./components/ FileUpload.js";
 import ContactUpload from "./components/ContactUpload.js";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-//add the fn
 function App() {
   return (
     <BrowserRouter>
