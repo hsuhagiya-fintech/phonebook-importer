b73bb158f03ec25b0c2014a47d85a62f81874c2e
hsuhagiyafintech <harikrushna.suhagiya@fintechglobal.center>
2025-08-04 22:29:14 +0530

    d

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import excelReducer from "./slices/excelSlice";
-
-export const store = configureStore({
-  reducer: {
-    excel: excelReducer,
-  },
-});
