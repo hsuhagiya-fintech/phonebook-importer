9e3aff6bc31b198f74724826be628a1d0a63548b
hsuhagiyafintech <harikrushna.suhagiya@fintechglobal.center>
2025-08-05 18:02:00 +0530

    add

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+//added
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
