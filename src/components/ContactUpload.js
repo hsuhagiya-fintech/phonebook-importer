ae5ae8fa57848023ee03904de8598f9ccd3df93f
hsuhagiyafintech <harikrushna.suhagiya@fintechglobal.center>
2025-08-05 22:14:00 +0530

    add

--- src/components/ContactUpload.js
+++ src/components/ContactUpload.js
@@ -1,32 +1,32 @@
-import "./ContactUpload.css";
-import CreateTableForContactUpload from "./CreateTableForContactUpload";
-import { useNavigate,useLocation } from "react-router-dom";
-export default function ContactUpload() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const file = location.state?.file;
+// import "./ContactUpload.css";
+// import CreateTableForContactUpload from "./CreateTableForContactUpload";
+// import { useNavigate,useLocation } from "react-router-dom";
+// export default function ContactUpload() {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   const file = location.state?.file;
 
 
-  return (
-    <>
-      <div className="header-container">
-        <button className="header-button" onClick={() => navigate("/")}>
-          ← Back to Upload
-        </button>
+//   return (
+//     <>
+//       <div className="header-container">
+//         <button className="header-button" onClick={() => navigate("/")}>
+//           ← Back to Upload
+//         </button>
 
-        <div className="header-title-group">
-          <h1 className="header-title">Phonebook Data</h1>
-          <p className="header-subtitle">Manage your imported contacts</p>
-        </div>
+//         <div className="header-title-group">
+//           <h1 className="header-title">Phonebook Data</h1>
+//           <p className="header-subtitle">Manage your imported contacts</p>
+//         </div>
 
-        <button className="header-button" onClick={() => navigate("/")}>
-          New Upload
-        </button>
-      </div>
+//         <button className="header-button" onClick={() => navigate("/")}>
+//           New Upload
+//         </button>
+//       </div>
 
-      <div>
-        <CreateTableForContactUpload/>
-      </div>
-    </>
-  );
-}
+//       <div>
+//         <CreateTableForContactUpload/>
+//       </div>
+//     </>
+//   );
+// }
