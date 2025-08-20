02dcf7c4483d136de0308cb43b158003945b39be
hsuhagiyafintech <111632888+harikrushna0@users.noreply.github.com>
2025-08-20 14:49:56 +0530

    add

--- src/components/change.js
+++ src/components/change.js
@@ -260,3 +260,95 @@ export default {};
 
 
 
+
+// ---- Logger ----
+export class Logger {
+  constructor(scope = 'app') {
+    this.scope = scope;
+    this.level = 'info';
+  }
+  fmt(level, msg) {
+    const ts = new Date().toISOString();
+    return `[${ts}] [${this.scope}] [${level}] ${msg}`;
+  }
+  info(msg) { console.log(this.fmt('info', msg)); }
+  warn(msg) { console.warn(this.fmt('warn', msg)); }
+  error(msg) { console.error(this.fmt('error', msg)); }
+  debug(msg) { if (this.level === 'debug') console.debug(this.fmt('debug', msg)); }
+}
+
+// ---- Collection Helpers ----
+export const groupBy = (arr, keyFn) => arr.reduce((acc, item) => {
+  const k = keyFn(item);
+  (acc[k] ||= []).push(item);
+  return acc;
+}, {});
+export const chunk = (arr, size) => {
+  const out = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+};
+export const uniq = (arr) => Array.from(new Set(arr));
+export const range = (n, start = 0) => Array.from({ length: n }, (_, i) => i + start);
+export const sum = (arr) => arr.reduce((a, b) => a + b, 0);
+export const average = (arr) => arr.length ? sum(arr) / arr.length : 0;
+
+// ---- SimpleEventEmitter ----
+export class SimpleEventEmitter {
+  constructor() {
+    this.map = new Map();
+  }
+  on(evt, handler) {
+    const list = this.map.get(evt) || [];
+    list.push(handler);
+    this.map.set(evt, list);
+    return () => this.off(evt, handler);
+  }
+  off(evt, handler) {
+    const list = this.map.get(evt) || [];
+    const i = list.indexOf(handler);
+    if (i >= 0) list.splice(i, 1);
+  }
+  emit(evt, ...args) {
+    const list = this.map.get(evt) || [];
+    for (const fn of list) try { fn(...args); } catch (_) {}
+  }
+}
+
+// padding line 84
+// padding line 85
+// padding line 86
+// padding line 87
+// padding line 88
+// padding line 89
+// padding line 90
+// padding line 91
+// padding line 92
+// padding line 93
+// padding line 94
+// padding line 95
+// padding line 96
+// padding line 97
+// padding line 98
+// padding line 99
+// padding line 100
+// padding line 101
+// padding line 102
+// padding line 103
+// padding line 104
+// padding line 105
+// padding line 106
+// padding line 107
+// padding line 108
+// padding line 109
+// padding line 110
+// padding line 111
+// padding line 112
+// padding line 113
+// padding line 114
+// padding line 115
+// padding line 116
+// padding line 117
+// padding line 118
+// padding line 119
+export default {};
